46748351718875f348e7a83c0200006eb0a9c09b
Tinnaphop Choonchuachan <mistertfy64@mistertfy64.com>
2023-05-18 18:50:12 +0700

    change: remove uneccessary comments

--- src/index.ts
+++ src/index.ts
@@ -2,24 +2,21 @@ import express from "express";
 import * as dotenv from "dotenv";
 dotenv.config();
 import fs from "fs";
-import bodyParser from "body-parser";
+import cors from "cors";
 import path from "path";
+
+// cors
+const corsOptions = {
+    origin: "http://localhost:3000",
+};
+
 // app
 const app: express.Application = express().disable("x-powered-by");
-
-// vars
-let files: Array<string> = [];
-
+app.use(cors(corsOptions));
 // routes and folders as sub paths
 walk(path.join(__dirname, "routes")).forEach((file: string) => {
     let path = file.substring("./routes".length);
     path = path.substring(0, path.length - 3);
-    // app[route.httpMethod as string](
-    //     path.replace("_", ":"),
-    //     async (req: express.Request, res: express.Response) => {
-    //         await route.run(this, req, res);
-    //     }
-    // );
     app.use(require(file).router);
 });
 
@@ -34,7 +31,6 @@ function walk(dir: string) {
     let list = fs.readdirSync(path.join(dir));
     list.forEach(function (file) {
         let fileToStat = path.join(dir, file);
-        console.log(fileToStat);
         let stat = fs.statSync(fileToStat);
         if (stat && stat.isDirectory()) {
             results = results.concat(walk(fileToStat));

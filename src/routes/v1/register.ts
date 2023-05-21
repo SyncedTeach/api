903ea3ae54c83519c26dc9d8f40fc28e1c4e449d
Pat <72650902+EpicCatto@users.noreply.github.com>
2023-05-21 21:33:36 +0700

    register requirement

--- src/routes/v1/register.ts
+++ src/routes/v1/register.ts
@@ -14,6 +14,24 @@ router.post(
             });
         }
 
+        if(req.body.username.length < 3 || req.body.username.length > 20) {
+            return res.status(400).json({
+                error: "Username must be between 3 and 20 characters!",
+            });
+        }
+
+        if(req.body.password.length < 8 || req.body.password.length > 20) {
+            return res.status(400).json({
+                error: "Password must be between 8 and 20 characters!",
+            });
+        }
+
+        if(req.body.personal_email.length < 3 || req.body.personal_email.length > 50) {
+            return res.status(400).json({
+                error: "Email must be between 3 and 50 characters!",
+            });
+        }
+
         console.log("register request!");
         res.json(await register(req.body.username, req.body.password, req.body.personal_email));
     }

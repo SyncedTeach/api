8674dc4247f90c447ecf02766816bc3c6c6d72f0
Tinnaphop Choonchuachan <mistertfy64@mistertfy64.com>
2023-05-22 10:22:30 +0700

    change: refactored `register.ts`

--- src/routes/v1/register.ts
+++ src/routes/v1/register.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import { checkHTML, checkMongoDB } from "../../utilities/sanitize";
 import { register } from "../../services/authenticate";
 import bodyParser from "body-parser";
 var jsonParser = bodyParser.json();
@@ -7,33 +8,56 @@ router.post(
     "/v1/register",
     jsonParser,
     async (req: express.Request, res: express.Response) => {
+        let username = req.body.username;
+        let password = req.body.password;
+        let email = req.body.personal_email;
+
         // filter out bad requests
-        if (!req.body.username || !req.body.password || !req.body.personal_email) {
+        if (!checkHTML(username) || !checkMongoDB(username)) {
             return res.status(400).json({
                 error: "Bad request!",
             });
         }
 
-        if(req.body.username.length < 3 || req.body.username.length > 20) {
+        if (!checkHTML(password) || !checkMongoDB(password)) {
+            return res.status(400).json({
+                error: "Bad request!",
+            });
+        }
+
+        if (!checkHTML(email) || !checkMongoDB(email)) {
+            return res.status(400).json({
+                error: "Bad request!",
+            });
+        }
+
+        if (!username || !password || !email) {
+            return res.status(400).json({
+                error: "Bad request!",
+            });
+        }
+
+        // actually process request
+        if (username.length < 3 || username.length > 20) {
             return res.status(400).json({
                 error: "Username must be between 3 and 20 characters!",
             });
         }
 
-        if(req.body.password.length < 8 || req.body.password.length > 20) {
+        if (password.length < 8 || password.length > 20) {
             return res.status(400).json({
                 error: "Password must be between 8 and 20 characters!",
             });
         }
 
-        if(req.body.personal_email.length < 3 || req.body.personal_email.length > 50) {
+        if (email.length < 3 || email.length > 50) {
             return res.status(400).json({
                 error: "Email must be between 3 and 50 characters!",
             });
         }
 
         console.log("register request!");
-        res.json(await register(req.body.username, req.body.password, req.body.personal_email));
+        res.json(await register(username, password, email));
     }
 );
 

44cee7dfebcebfaf7ad2dd415eed95e0d991ef16
Tinnaphop Choonchuachan <mistertfy64@mistertfy64.com>
2023-05-22 17:48:46 +0700

    change: refactor `checkToken.ts`

--- src/routes/v1/checkToken.ts
+++ src/routes/v1/checkToken.ts
@@ -1,13 +1,17 @@
 import express from "express";
 import { checkOwnerOfToken } from "../../services/token";
 import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
 var jsonParser = bodyParser.json();
 var router = express.Router();
 router.post(
     "/v1/checkToken",
-    jsonParser,
+    [jsonParser, cookieParser()],
     async (req: express.Request, res: express.Response) => {
-        let result = await checkOwnerOfToken(req.body.token, req.body.username);
+        let result = await checkOwnerOfToken(
+            req.cookies.token,
+            req.cookies.username
+        );
         if (!result.success) {
             return res.status(401).json(result);
         }

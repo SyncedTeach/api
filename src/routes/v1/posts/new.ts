6eb75e6a6fb74f0c0353f6dbffcc9528732e053a
Tinnaphop Choonchuachan <mistertfy64@mistertfy64.com>
2023-05-22 19:12:45 +0700

    feature: the ability to add posts

---
+++ src/routes/v1/posts/new.ts
@@ -0,0 +1,47 @@
+import express from "express";
+import { checkOwnerOfToken } from "../../../services/token";
+import { isAdmin } from "../../../services/authorize";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import { checkHTML, checkMongoDB } from "../../../utilities/sanitize";
+import { addPost } from "../../../services/post";
+var jsonParser = bodyParser.json();
+var router = express.Router();
+// TODO: add logging
+router.post(
+    "/v1/posts/new",
+    [jsonParser, cookieParser()],
+    async (req: express.Request, res: express.Response) => {
+        let content = req.body["post-content"];
+        let result = {
+            success: false,
+        };
+        // check if user is real
+        let cookies = req.cookies;
+        let cookieResult = await checkOwnerOfToken(
+            cookies.sessionToken,
+            cookies.username
+        );
+        if (!cookieResult.success) {
+            res.json(result);
+            return result;
+        }
+        // check if user has permissions
+        let username = cookies.username;
+        let rankResult = await isAdmin(username);
+        if (!rankResult) {
+            res.json(result);
+            return result;
+        }
+        // sanitize/validate data
+        if (!checkHTML(content) || !checkMongoDB(content)) {
+            res.json(result);
+            return result;
+        }
+        addPost(content, username);
+        result.success = true;
+        res.json(result);
+        return result;
+    }
+);
+export { router };
---
+++ src/services/post.ts
@@ -0,0 +1,20 @@
+import { Post, IPost } from "../models/Post";
+// TODO: add title
+async function addPost(content: string, username: string) {
+    let dateTime = Date.now();
+    let post = new Post({
+        owner: username,
+        content: content,
+        dateTime: dateTime,
+        lastEditDateTime: dateTime,
+    });
+    await post.save();
+}
+
+async function findPost(id: string) {
+    // id is already validated, but it might be a good idea to validate it once more:
+    let post = await Post.findOne({ _id: id });
+    return post;
+}
+
+export { addPost, findPost };

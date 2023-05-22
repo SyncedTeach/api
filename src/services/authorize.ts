3ad4f369826751a7195384c4a81e6c0c5220fd04
Tinnaphop Choonchuachan <mistertfy64@mistertfy64.com>
2023-05-22 17:49:16 +0700

    change: add authorization functions

--- src/services/authorize.ts
+++ src/services/authorize.ts
@@ -1,1 +1,51 @@
 import { User } from "../models/User";
+import { checkHTML, checkMongoDB } from "../utilities/sanitize";
+
+// TODO: Add more overloads
+// NOTE: isAdmin is either normal or super admin
+async function isAdmin(username: string) {
+    let info = {
+        message: "",
+        success: false,
+    };
+    if (!checkHTML(username) || !checkMongoDB(username)) {
+        info.message = "Invalid username!";
+        return info;
+    }
+    let user = await User.findOne({ username: username });
+    if (!user) {
+        info.message = "User not found!";
+        return info;
+    }
+
+    if (
+        user.membership.isAdministrator ||
+        user.membership.isSuperAdministrator
+    ) {
+        info.success = true;
+    }
+    return info;
+}
+
+async function isSuperAdmin(username: string) {
+    let info = {
+        message: "",
+        success: false,
+    };
+    if (!checkHTML(username) || !checkMongoDB(username)) {
+        info.message = "Invalid username!";
+        return info;
+    }
+    let user = await User.findOne({ username: username });
+    if (!user) {
+        info.message = "User not found!";
+        return info;
+    }
+
+    if (user.membership.isSuperAdministrator) {
+        info.success = true;
+    }
+    return info;
+}
+
+export { isSuperAdmin, isAdmin };

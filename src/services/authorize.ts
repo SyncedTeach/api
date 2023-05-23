e7bfdfe8bec3cc79f13256f3d73447455a453673
Tinnaphop Choonchuachan <mistertfy64@mistertfy64.com>
2023-05-23 21:11:13 +0700

    change: changed how ranks/roles work

--- src/services/authorize.ts
+++ src/services/authorize.ts
@@ -1,6 +1,19 @@
 import { User } from "../models/User";
 import { checkHTML, checkMongoDB } from "../utilities/sanitize";
-
+const RANKS = {
+    superAdministrator: "isSuperAdministrator",
+    administrator: "isAdministrator",
+    teacher: "isTeacher",
+    student: "isStudent",
+    parent: "isParent",
+};
+enum Rank {
+    superAdministrator = "superAdministrator",
+    administrator = "administrator",
+    teacher = "teacher",
+    student = "student",
+    parent = "parent",
+}
 // TODO: Add more overloads
 // NOTE: isAdmin is either normal or super admin
 async function isAdmin(username: string) {
@@ -48,10 +61,33 @@ async function isSuperAdmin(username: string) {
     return info;
 }
 
-async function checkRank(username: string, criteria: number) {
+async function checkRank(username: string, rank: string | string[]) {
     let result = {
         success: false,
     };
+    if (!checkHTML(username) || !checkMongoDB(username)) {
+        return result;
+    }
+    let ranks = await getRanks(username);
+    if (ranks.length === 0) {
+        return result;
+    }
+    // case 1: if rank is a string
+    if (typeof rank === "string") {
+        result.success = ranks.indexOf(rank) > -1;
+        return result;
+    }
+    // case 2: if rank is an array
+    for (let currentRank of rank) {
+        if (ranks.indexOf(currentRank) > -1) {
+            result.success = true;
+        }
+    }
+    return result;
+}
+
+async function getRanks(username: string) {
+    let result: Array<string> = [];
     if (!checkHTML(username) || !checkMongoDB(username)) {
         return result;
     }
@@ -59,17 +95,25 @@ async function checkRank(username: string, criteria: number) {
     if (!user) {
         return result;
     }
-    let rank = 1;
-    if (user.membership.isAdministrator) {
-        rank = 2;
+    // TODO: Clean this
+    // I can't find a better way -mistertfy64
+    let membership = user.membership;
+    if (membership.isSuperAdministrator) {
+        result.push("superAdministrator");
     }
-    if (user.membership.isSuperAdministrator) {
-        rank = 3;
+    if (membership.isAdministrator) {
+        result.push("administrator");
     }
-    if (rank >= criteria) {
-        result.success = true;
+    if (membership.isTeacher) {
+        result.push("teacher");
+    }
+    if (membership.isStudent) {
+        result.push("student");
+    }
+    if (membership.isParent) {
+        result.push("parent");
     }
     return result;
 }
 
-export { isSuperAdmin, isAdmin, checkRank };
+export { isSuperAdmin, isAdmin, checkRank, RANKS };

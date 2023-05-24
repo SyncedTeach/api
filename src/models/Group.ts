0e34bd8e576afefc32c7211a59de819e1096ac93
Tinnaphop Choonchuachan <mistertfy64@mistertfy64.com>
2023-05-24 13:55:36 +0700

    change: added group visibility + join codes

--- src/models/Group.ts
+++ src/models/Group.ts
@@ -4,12 +4,16 @@ interface IGroup {
   name: string;
   members: Array<Schema.Types.ObjectId>[];
   owners: Array<Schema.Types.ObjectId>[];
+  joinCode: string;
+  private: boolean;
 }
 
 const groupSchema = new Schema<IGroup>({
   name: String,
   members: Array<Schema.Types.ObjectId>,
   owners: Array<Schema.Types.ObjectId>,
+  joinCode: String,
+  private: Boolean,
 });
 
 const Group = model<IGroup>("Group", groupSchema, "groups");
--- src/services/group.ts
+++ src/services/group.ts
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 import { Group, IGroup } from "../models/Group";
+import { randomBytes } from "crypto";
 // TODO: add title
 async function addGroup(
   name: string,
@@ -9,10 +10,14 @@ async function addGroup(
   if (typeof userID === "string") {
     userID = new mongoose.Types.ObjectId(userID);
   }
+  let bytes = await randomBytes(16);
   let group = new Group({
     owners: [userID],
     members: [userID],
     name: name,
+    // TODO: consider the use of randomBytes on whether its necessary.
+    joinCode: bytes.toString("hex"),
+    private: false,
   });
   await group.save();
 }

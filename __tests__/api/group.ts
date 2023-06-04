5cb6394ea4b5f1390c1ffd137a44e13e318c6087
Tinnaphop Choonchuachan <mistertfy64@mistertfy64.com>
2023-06-04 07:52:25 +0700

    change: add more test cases

--- __tests__/api/group.ts
+++ __tests__/api/group.ts
@@ -8,7 +8,23 @@ describe("Group system", () => {
     const response = await request(testApp)
       .post("/v1/groups/new")
       .set("Cookie", ["username=admin-tester", "sessionToken=sessionToken123"])
-      .send({ "group-name": "test 1" });
+      .send({ "group-name": "test 1 good" });
     expect(response.body.success).toBe(true);
+  });
+
+  it("should not let users w/o permissions create new groups", async () => {
+    const response = await request(testApp)
+      .post("/v1/groups/new")
+      .set("Cookie", ["username=normal-tester", "sessionToken=sessionToken456"])
+      .send({ "group-name": "test 2 bad" });
+    expect(response.body.success).toBe(false);
+  });
+
+  it("should not let users w/ incorrect credentials create new groups", async () => {
+    const response = await request(testApp)
+      .post("/v1/groups/new")
+      .set("Cookie", ["username=admin-tester", "sessionToken=badSessionToken"])
+      .send({ "group-name": "test 3 bad" });
+    expect(response.body.success).toBe(false);
   });
 });

0f7942e2b0abddf984837038a471587d27b420e6
Tinnaphop Choonchuachan <mistertfy64@mistertfy64.com>
2023-06-10 18:00:55 +0700

    fix: fixed tests

--- __tests__/api/group.ts
+++ __tests__/api/group.ts
@@ -24,7 +24,7 @@ describe("Group system", () => {
     const response = await request(testApp)
       .post("/v1/groups/new")
       .set("Cookie", ["username=admin-tester", "sessionToken=sessionToken123"])
-      .send({ "group-name": "test 1 good" });
+      .send({ name: "test 1 good" });
     expect(response.body.success).toBe(true);
   });
 
@@ -32,7 +32,7 @@ describe("Group system", () => {
     const response = await request(testApp)
       .post("/v1/groups/new")
       .set("Cookie", ["username=normal-tester", "sessionToken=sessionToken456"])
-      .send({ "group-name": "test 2 bad" });
+      .send({ name: "test 2 bad" });
     expect(response.body.success).toBe(false);
   });
 
@@ -40,7 +40,7 @@ describe("Group system", () => {
     const response = await request(testApp)
       .post("/v1/groups/new")
       .set("Cookie", ["username=admin-tester", "sessionToken=badSessionToken"])
-      .send({ "group-name": "test 3 bad" });
+      .send({ name: "test 3 bad" });
     expect(response.body.success).toBe(false);
   });
 
